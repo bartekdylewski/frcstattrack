0311ae08ed83cdfd1f5e2279efcb87f6a2393dde
Bartłomiej Dylewski <dylewskipoczta@gmail.com>
2023-01-24 21:10:53 +0100

    Messy Working
    Working code, writes to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,13 @@ import edu.wpi.first.networktables.NetworkTablesJNI;
 import edu.wpi.first.util.CombinedRuntimeLoader;
 import edu.wpi.first.util.WPIUtilJNI;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import com.opencsv.CSVWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -62,11 +67,44 @@ public class Main {
       NetworkTable fms = inst.getTable("FMSInfo");
       
       // Get values
-      DoubleSubscriber x = sd.getDoubleTopic("Left Distance in xxxxx").subscribe(0);
-      DoubleSubscriber y = sd.getDoubleTopic("Right Distance in xxxxx").subscribe(0);
-      DoubleSubscriber z = fms.getDoubleTopic("FMSControlData").subscribe(0);
+      DoubleSubscriber leftDistance = sd.getDoubleTopic("Left Distance in xxxxx").subscribe(0);
+      DoubleSubscriber rightDistance = sd.getDoubleTopic("Right Distance in xxxxx").subscribe(0);
+      DoubleSubscriber fmsControlData = fms.getDoubleTopic("FMSControlData").subscribe(0);
 
 
+      // Create writer
+      // default all fields are enclosed in double quotes
+      // default separator is a comma
+      try (CSVWriter writer = new CSVWriter(new FileWriter("test.csv", true))) {
+        String[] header = {"siema", "elo"};
+        writer.writeNext(header, false);
+        System.out.println("HEADER ADDED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+      }
+      
+      System.out.println("1111111");;
+
+      int id = 0;
+      while(true){
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+          System.out.println("interrupted");
+          return;
+        }
+
+
+        String ld = String.valueOf(leftDistance.get());
+        try (CSVWriter writer = new CSVWriter(new FileWriter("test.csv", true))) {
+        updateCsv(writer, id, ld);
+        }
+        
+        System.out.println(ld);
+        System.out.println("id: " + id);
+
+        id++;
+      }
+    
+/* 
       // Print loop
       while (true) {
         try {
@@ -75,8 +113,22 @@ public class Main {
           System.out.println("interrupted");
           return;
         }
-        System.out.println("leftD: " + x.get() + " rightD: " + y.get() + " FMSControlData: " + z.get());
-      }
 
+        System.out.println("leftD: " + leftDistance.get() + " rightD: " + rightDistance.get() + " FMSControlData: " + fmsControlData.get());
+      }
+ */
+    }
+
+    private static void setupCsvHeader(CSVWriter writer) {
+      String[] header = {"siema", "elo"};
+      writer.writeNext(header, false);
+      System.out.println("HEADER ADDED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+
+    private static void updateCsv(CSVWriter writer, int id, String ld) {
+
+      String[] entry = {Integer.toString(id), ld};
+      writer.writeNext(entry);
+      System.out.println("entry updated");
     }
 }
